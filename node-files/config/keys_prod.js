5652e05069f2ee1c644c40d868b6d29222d1ce6f
adele-angel <adeler.ang@gmail.com>
2020-05-16 20:06:51 +0300

    Fix config files

--- node-files/config/keys_prod.js
+++ node-files/config/keys_prod.js
@@ -1,7 +1,6 @@
 module.exports = {
 	port: process.env.PORT,
 	mongoURI: process.env.MONGO_URI,
-	mongoDB: process.env.MONGO_DB,
 	redisURI: process.env.REDIS_URI,
 	redisPORT: process.env.REDIS_PORT,
 	redisAUTH: process.env.REDIS_AUTH,
