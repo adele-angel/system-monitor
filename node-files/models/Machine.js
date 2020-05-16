058a516cd7887ffa81bbcc365e00a69ae3e29233
adele-angel <adeler.ang@gmail.com>
2020-05-16 20:21:51 +0300

    Creating a mongo schema (model)

---
+++ node-files/models/Machine.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Machine = new Schema({
+	macA: String,
+	cpuLoad: Number,
+	freeMem: Number,
+	totalMem: Number,
+	usedMem: Number,
+	memUsage: Number,
+	osType: String,
+	upTime: Number,
+	cpuModel: String,
+	numCores: Number,
+	cpuSpeed: Number,
+});
+
+module.exports = mongoose.model('Machine', Machine);
